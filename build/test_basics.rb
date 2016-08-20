a3f4d7a9d031d8e9ce0bbfa94c8ac193f30561fc
Daniel Knoppel (Phusion) <daniel@phusion.nl>
2016-08-20 12:06:58 +0200

    hello again bluecloth error, let's try all of this with 1.11.2

--- build/test_basics.rb
+++ build/test_basics.rb
@@ -57,7 +57,7 @@ task 'test:install_deps' do
   end
 
   if !PlatformInfo.locate_ruby_tool('bundle') || bundler_too_old?
-    sh "gem uninstall bundler bundle rubygems-update --all -x && gem update --system 2.6.1 && gem install bundler && gem uninstall rubygems-update --all -x && gem update --system 2.6.4"
+    sh "gem uninstall bundler bundle rubygems-update --all -x && gem update --system 2.6.1 && gem install bundler --version 1.11.2 && gem uninstall rubygems-update --all -x && gem update --system 2.6.4"
     sh "rvm install ruby-2.2.0"
     # workaround for issue "bluecloth not found" when using 1.12.x
     #sh "#{gem_install} bundler --version 1.11.2"
